ac34ed9a3f4f102127e4942601b333956fb71cda
Sergio Maass <sergio@postcenter.io>
2017-12-01 17:05:31 -0300

    Send message example

--- example.py
+++ example.py
@@ -5,7 +5,9 @@ if __name__ == '__main__':
         'webdriver/chromedriver',
         'data/whatsapp-driver'
     )
-    messages = driver.get_unread_messages()
-    for msg in messages:
-        print(msg.text)
+    # messages = driver.get_unread_messages()
+    # for msg in messages:
+    #     print(msg.text)
+    driver.send_message('56999645308', 'Oli, soy un bot')
     driver.screenshot('demo.png')
+    driver.quit()
