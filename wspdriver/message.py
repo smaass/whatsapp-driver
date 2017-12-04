3b105767afee087c4b962ff0b6383bd4950bb573
Sergio Maass <sergio@postcenter.io>
2017-12-04 17:44:02 -0300

    Adapted to breaking changes: data-id was removed from message elements :(

--- wspdriver/message.py
+++ wspdriver/message.py
@@ -1,3 +1,4 @@
+import hashlib
 import re
 
 from datetime import datetime
@@ -6,8 +7,6 @@ from datetime import datetime
 class WhatsappMessage(object):
 
     METADATA_REGEX = re.compile(r'\[(\d+):(\d+),\s(\d+)/(\d+)/(\d+)\]\s(.*):')
-    SINGLE_CHAT_MID_REGEX = re.compile(r'(false|true)_(\d+)@(.*)')
-    GROUP_CHAT_MID_REGEX = re.compile(r'(false|true)_(\d+-\d+)@(.*)_(\d+)')
 
     def __init__(self, message_element):
 
@@ -15,13 +14,26 @@ class WhatsappMessage(object):
         metadata = bubble.get_attribute('data-pre-plain-text')
         self.datetime, self.author_name = self.parse_metadata(metadata)
 
+        classes = message_element.get_attribute('class')
+        self.is_outbound = 'message-out' in classes
+
         bubble_text = message_element.find_element_by_css_selector(
-            '.bubble > .message-text'
+            '.bubble .selectable-text'
         )
         self.text = bubble_text.text
 
-        mid = bubble_text.get_attribute('data-id')
-        self.is_outbound, self.id, self.author_phone = self.parse_mid(mid)
+        to_hash = '{}{}{}'.format(
+            self.datetime, self.author_name, self.text
+        )
+        self.id = hashlib.md5(to_hash.encode()).hexdigest()
+
+    def __str__(self):
+
+        return '[{}] {}: {}'.format(
+            self.datetime,
+            'Me' if self.is_outbound else self.author_name,
+            self.text
+        )
 
     @classmethod
     def parse_metadata(cls, data_text):
@@ -35,30 +47,3 @@ class WhatsappMessage(object):
         dt = datetime(year, month, day, hour, minute)
         name = match.group(6)
         return dt, name
-
-    @classmethod
-    def parse_group_chat_mid(cls, regex_match):
-
-        is_outbound = regex_match.group(1) == 'true'
-        group_id = regex_match.group(2)
-        mid = regex_match.group(3)
-        phone = regex_match.group(4)
-        return is_outbound, group_id + mid, phone
-
-    @classmethod
-    def parse_single_chat_mid(cls, regex_match):
-
-        is_outbound = regex_match.group(1) == 'true'
-        phone = regex_match.group(2)
-        mid = regex_match.group(3)
-        return is_outbound, mid, phone
-
-    @classmethod
-    def parse_mid(cls, message_id):
-
-        regex_match = re.match(cls.SINGLE_CHAT_MID_REGEX, message_id)
-        if regex_match is None:
-            regex_match = re.match(cls.GROUP_CHAT_MID_REGEX, message_id)
-            return cls.parse_group_chat_mid(regex_match)
-        else:
-            return cls.parse_single_chat_mid(regex_match)
