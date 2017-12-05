d10f551cd5775e259b2a91cdccdf5e549da03d76
Sergio Maass <sergio@postcenter.io>
2017-12-05 12:20:12 -0300

    Improved example

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from wspdriver.driver import WhatsappDriver
 
 if __name__ == '__main__':
+
     print('INITIALIZING...')
     driver = WhatsappDriver.start(
         'webdriver/chromedriver',
@@ -8,12 +9,20 @@ if __name__ == '__main__':
     )
     if not driver.is_logged_in():
         print('MUST LOG IN')
-        driver.log_in()
+        driver.save_login_as_image('login.png')
+        driver.wait_for_login()
         print('LOGGED IN')
+
     # messages = driver.get_unread_messages()
     # for msg in messages:
     #     print(msg.text)
-    print('SENDING MESSAGE')
-    driver.send_message('56999645308', 'Oli, soy un bot')
+    # print('SENDING MESSAGE')
+    # driver.send_message('56999645308', 'Oli, soy un bot')
+
+    chats = driver.get_chats()
+    chat = chats.__next__()
+    for message in chat.get_messages():
+        print(message.id)
+        print(message)
     driver.screenshot('demo.png')
     driver.quit()
